c92944e5f287ab4aa716a21a6534fc179cb6b6cc
Jon de la Motte <jon.delamotte@vendscreen.com>
2014-05-28 23:46:23 -0700

    Small change, added a ctags config file

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ ln -sf $HOME/.jvim/.vimrc $HOME/.vimrc
 # Bonus
 ln -sf $HOME/.jvim/bonus/.tmux.conf $HOME/.tmux.conf
 ln -sf $HOME/.jvim/bonus/.zshrc $HOME/.zshrc
+ln -sf $HOME/.jvim/bonus/.ctags $HOME/.ctags
